47fa418ca0d2b3ed0a9964acd40c8da0d6ab10a7
Jose Camata <camata@ice.ufjf.br>
2022-05-19 12:03:04 -0300

    update lab04

---
+++ siga/include/config.h
@@ -0,0 +1,6 @@
+/* #undef CLASSROOM */
+#define CPU_TIME
+
+#define INPUT_DIR "/home/camata/git/dcc012/dcc012b-lab04-heap/tests/input/"
+#define EXPECTED_OUTPUT_DIR "/home/camata/git/dcc012/dcc012b-lab04-heap/tests/expected_output/"
+
