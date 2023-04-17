58e9e419f6e0414e4da06217b388613441bdb2e7
Jose Camata <72111360+josecamata@users.noreply.github.com>
2023-04-17 01:22:00 +0000

    add solution

--- siga/include/config.h
+++
@@ -1,6 +0,0 @@
-/* #undef CLASSROOM */
-#define CPU_TIME
-
-#define INPUT_DIR "/home/camata/git/dcc012/dcc012b-lab04-heap/tests/input/"
-#define EXPECTED_OUTPUT_DIR "/home/camata/git/dcc012/dcc012b-lab04-heap/tests/expected_output/"
-
